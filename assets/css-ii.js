ba1f49f118980dbd585d73b07f2b20895e1df3b9
Diego Carbone <diegocarbone@gmail.com>
2018-07-03 13:32:51 -0300

    agregado subrayado botones menu / solapas en section

--- assets/css-ii.js
+++ assets/css-ii.js
@@ -6,9 +6,28 @@ $( document ).ready(function() {
 		$("#menu").toggleClass('open');
 	});
 
+	$("#menu a").click(function(e) {
+		e.preventDefault();
+		$("#hamb").toggleClass('open');
+		$("#menu").toggleClass('open');
+	});
+
 	$("#side").click(function(e) {
 		e.preventDefault();
 		$("#side").toggleClass('open');
+	});
+
+
+	$("#solapas li:first-of-type a").click(function(e) {
+		e.preventDefault();
+		$("section").removeClass('visible');
+		$("section:first-of-type").addClass('visible');
+	});
+
+	$("#solapas li:last-of-type a").click(function(e) {
+		e.preventDefault();
+		$("section").removeClass('visible');
+		$("section:last-of-type").addClass('visible');
 	});
 
 
